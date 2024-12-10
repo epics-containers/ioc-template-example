35b25344cb01508173c3c655703442a4b835eff9
gilesknap <gilesknap@users.noreply.github.com>
2024-12-10 07:57:23 +0000

    deploy: epics-containers/ioc-template@ed64dd731631f8b50a85007ed1d9dbba1559db94

--- opi/phoebus-launch.sh
+++ opi/phoebus-launch.sh
@@ -17,12 +17,12 @@ settings="
 -settings /tmp/settings.ini
 "
 
-if which phoebus.sh &>/dev/null ; then
+if which phoebus.sh &>/dev/null && [[ -z ${use_container} ]] ; then
     echo "Using phoebus.sh from PATH"
     set -x
     phoebus.sh ${settings} "${@}"
 
-elif module load phoebus 2>/dev/null; then
+elif module load phoebus 2>/dev/null && [[ -z ${use_container} ]] ; then
     echo "Using phoebus module"
     set -x
     phoebus.sh ${settings} "${@}"
@@ -33,16 +33,14 @@ else
     # prefer podman but use docker if USE_DOCKER is set
     if podman version &> /dev/null && [[ -z $USE_DOCKER ]]
         then docker=podman; UIDGID=0:0
-        else docker=docker; UIDGID=$(id -u):$(id -g)
+        else docker=docker; UIDGID=$(id -u):$(id -g); xhost +SI:localuser:$(id -un)
     fi
     echo "Using $docker as container runtime"
 
-    # ensure local container users can access X11 server
-    xhost +SI:localuser:$(id -un)
 
     # settings for container launch
     x11="-e DISPLAY --net host"
-    args="--rm -it --security-opt=label=none --user ${UIDGID}"
+    args="--rm -it --security-opt=label=disable --user ${UIDGID}"
     mounts="-v=/tmp:/tmp -v=${workspace}:/workspace -v=${workspace}/..:/workspaces"
     image="ghcr.io/epics-containers/ec-phoebus:latest"
 
